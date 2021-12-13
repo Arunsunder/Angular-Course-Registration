ab7734b5c07d52719357767aab85dd08f62cb881
Arun <subramaniarun.sj59@gmail.com>
2021-12-13 09:48:04 +0530

    Arun - Student Signup Form Reactive

--- src/app/components/student/studentsignup/studentsignup.component.ts
+++ src/app/components/student/studentsignup/studentsignup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl,FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-studentsignup',
@@ -8,8 +9,26 @@ import { Component, OnInit } from '@angular/core';
 export class StudentsignupComponent implements OnInit {
 
   constructor() { }
-
+  studentlogin!:FormGroup
+  DataID:number=0;
   ngOnInit(): void {
+    this.studentlogin=new FormGroup({
+      studentname :new FormControl(),
+      studentemail:new FormControl(),
+      studentmobile:new FormControl(),
+      studentdob:new FormControl(),
+      studentpassword:new FormControl()
+    });
+  }
+
+  Submit(form:FormGroup){
+    let formValues={
+       Name:this.studentlogin.value.input,
+       MobileNumber:this.studentlogin.value.input,
+       Email:this.studentlogin.value.input,
+       DOB:this.studentlogin.value.input,
+       Password:this.studentlogin.value.input
+    }
   }
 
 }
